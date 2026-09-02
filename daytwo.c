9de3f2260ad0650d4ee083df74d2a0fc0b5fc82b
Franklin Boachie-Yiadom <franklinboachie@gmail.com>
2026-09-02 22:56:10 +0500

    a simple nested for loop to create a shape

--- daytwo.c
+++ daytwo.c
@@ -4,20 +4,16 @@
 
 
 int main(){
- //These are for for loops
-
- //This is for increamenting from 1 to 10
-    int x;
-    for(x=1; x<=10; x++){
-        printf("%i\n",x);
+    int x,y,z;
+    for (x=5; x>=0; x--){
+        for (y= 0; y<x; y++){
+            printf("+");  
+        }
+        for (z=0; z<5-x; z++){
+            printf("*");
+        }
+        printf("\n");
     }
 
-    printf("\n");
-    
-//This is for decrementing from 10 to 1
-    for (x=10; x>=1; x--){
-        printf("%i\n" ,x);
-    } 
     return 0;
 }
-
